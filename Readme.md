7fba0b06cc125c67c14081dad869bd3586a045ec
SKGHD <sangramgorai@gmail.com>
2020-11-19 03:45:55 +0530

    added procfile

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# URL Shortner with NodeSJ, EXPRESS and MongoDB
+
+## Heroku Setup
+
+Add mongo driver key from mongo atlas to the heroku environvent variable.
+and done !
--- app.js
+++ app.js
@@ -1,16 +1,71 @@
 import express from "express";
+import mongoose from "mongoose";
 
+// importing database schema model
+import ShortURL from "./models/url.js";
 const app = express();
 
-const PORT = 1337;
+const PORT = 8080;
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-app.get("/", (request, response) => response.send("Hello from Homepage"));
+// routes
+app.get("/", async (req, res) => {
+  const allData = await ShortURL.find({}, { _id: 0 });
+  res.render("index", {
+    shortUrls: allData,
+  });
+});
 
-app.get("/short", (req, res) => res.send("You have reached Short route"));
+app.post("/short", async (req, res) => {
+  // Grabbing fullUrl from req.body
+  const url = req.body.fullUrl;
+  const record = new ShortURL({
+    full: url,
+  });
+  await record.save();
+  res.redirect("/");
+});
 
-// Listening for incoming requests
-app.listen(PORT, () =>
-  console.log(`Server Running on port: http://localhost:${PORT}`)
-);
+// shortid request route
+
+app.get("/:shortid", async (req, res) => {
+  // grabbing data from req
+  const shortid = req.params.shortid;
+  // finding the document
+  const data = await ShortURL.findOne({ short: shortid });
+
+  if (!data) {
+    return res.sendStatus(404);
+  }
+  // update clicks if data is not null
+  data.clicks++;
+  // save the data to db
+  await data.save();
+  // redirect user to original link
+  res.redirect(data.full);
+});
+
+// database connection
+
+const url = `mongodb+srv://skghd:LFTDdcsWYlI7eVlw@cluster0.u0gdz.mongodb.net/urlshortner?retryWrites=true&w=majority`;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+// Listening for incoming requests on successful connection to MongoAtlas
+mongoose.connection.on("open", () => {
+  app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
+});
